8cf235939af9a5f04a10d870f4b4831116e2d6bd
Gabriel Lopes <gabriel.lopes_glp@outlook.com>
2025-12-30 12:16:15 -0300

    🐛 fix text

--- update_daily.py
+++ update_daily.py
@@ -58,8 +58,8 @@ def update_daily_file():
     <language>pt-BR</language>
     <lastBuildDate>{pub}</lastBuildDate>
     <item>
-      <title><![CDATA[{daily_content['title']}]]></title>
-      <description><![CDATA[{daily_content['content']}]]></description>
+      <title><![CDATA[Texto do Dia]]></title>
+      <description><![CDATA[{daily_content['content']} — {daily_content['title']}]]></description>
       <link>hoje.json</link>
       <guid isPermaLink="false">{daily_content['full_date']}</guid>
       <pubDate>{pub}</pubDate>
